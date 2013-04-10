7ece4f919e181ba2cd999fdf732e96f6f78dbaba
Howard Chung <howardc93@gmail.com>
2013-04-10 01:32:31 -0400

    test

--- index.php
+++ index.php
@@ -31,11 +31,12 @@ if (! isset($_GET['oauth_verifier'])) {
     // store the authenticated token, which may be different from the request token (!)
     $_SESSION['oauth_token'] = $reply->oauth_token;
     $_SESSION['oauth_token_secret'] = $reply->oauth_token_secret;
-    $userId=$_SESSION['user_id'];
+    $userId=$reply->oauth_token;
     echo $userId;
-    echo $_SESSION['screen_name'];
+    echo $reply->screen_name;
+    $followers=(array) $cb->followers_ids($userId);
+	var_dump($followers);
 }
 
-$followers=(array) $cb->followers_ids($userId);
-var_dump($followers);
+
 ?>
