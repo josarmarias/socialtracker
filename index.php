86a8b195e68b0fdf8b450ca26febdfe44c448499
Howard Chung <howardc93@gmail.com>
2013-04-10 02:22:26 -0400

    test

--- index.php
+++ index.php
@@ -21,12 +21,8 @@ $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oau
 
 /* If method is set change API call made. Test is called by default. */
 $content = $connection->get('account/verify_credentials');
-
+$content2= $connection->get('followers/ids');
 /* Some example calls */
 //$connection->get('users/show', array('screen_name' => 'abraham'));
-//$connection->post('statuses/update', array('status' => date(DATE_RFC822)));
-//$connection->post('statuses/destroy', array('id' => 5437877770));
-//$connection->post('friendships/create', array('id' => 9436992));
-//$connection->post('friendships/destroy', array('id' => 9436992));
 
 include('html.inc');
