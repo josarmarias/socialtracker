4edf3bc6fed1094173d4156963429cb61bedc8f5
Howard Chung <howardc93@gmail.com>
2013-04-10 02:04:55 -0400

    test

--- config.php
+++ config.php
@@ -7,4 +7,4 @@
 
 define('CONSUMER_KEY', 'j1wLvQtdXPOaWN4HJVwKw');
 define('CONSUMER_SECRET', 'JJ0ahgla4s99b9h5GLVme0IeKkw3tOqT4fcp22F1Q');
-define('OAUTH_CALLBACK', 'http://socialtracker.herokuapp.com');
+define('OAUTH_CALLBACK', 'http://socialtracker.herokuapp.com/callback.php');
