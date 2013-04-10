356b523a85b634ad511b6b5192b65aa964840e23
Howard Chung <howardc93@gmail.com>
2013-04-10 01:58:57 -0400

    test

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'j1wLvQtdXPOaWN4HJVwKw');
+define('CONSUMER_SECRET', 'JJ0ahgla4s99b9h5GLVme0IeKkw3tOqT4fcp22F1Q');
+define('OAUTH_CALLBACK', 'http://socialtracker.herokuapp.com');
--- index.php
+++ index.php
@@ -1,42 +1,71 @@
 <?php
-require_once ('src/codebird.php');
-Codebird::setConsumerKey('j1wLvQtdXPOaWN4HJVwKw', 'JJ0ahgla4s99b9h5GLVme0IeKkw3tOqT4fcp22F1Q'); // static, see 'Using multiple Codebird instances'
+/**
+ * @file
+ * User has successfully authenticated with Twitter. Access tokens saved to session and DB.
+ */
 
-$cb = Codebird::getInstance();
-
-$cb->setToken($_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
-
+/* Load required lib files. */
 session_start();
-if (! isset($_GET['oauth_verifier'])) {
-    // gets a request token
-    $reply = $cb->oauth_requestToken(array(
-        'oauth_callback' => 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']
-    ));
+require_once('twitteroauth/twitteroauth.php');
+require_once('config.php');
 
-    // stores it
-    $cb->setToken($reply->oauth_token, $reply->oauth_token_secret);
-    $_SESSION['oauth_token'] = $reply->oauth_token;
-    $_SESSION['oauth_token_secret'] = $reply->oauth_token_secret;
-
-    // gets the authorize screen URL
-    $auth_url = $cb->oauth_authorize();
-    header('Location: ' . $auth_url);
-    die();
-
-} else {
-    // gets the access token
-    $cb->setToken($_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
-    $reply = $cb->oauth_accessToken(array(
-        'oauth_verifier' => $_GET['oauth_verifier']
-    ));
-    // store the authenticated token, which may be different from the request token (!)
-    $_SESSION['oauth_token'] = $reply->oauth_token;
-    $_SESSION['oauth_token_secret'] = $reply->oauth_token_secret;
-    $userId=$reply->user_id;
-    var_dump($userId);
-    var_dump($reply->screen_name);
+/* If access tokens are not available redirect to connect page. */
+if (empty($_SESSION['access_token']) || empty($_SESSION['access_token']['oauth_token']) || empty($_SESSION['access_token']['oauth_token_secret'])) {
+    header('Location: ./clearsessions.php');
 }
+/* Get user access tokens out of the session. */
+$access_token = $_SESSION['access_token'];
 
-    $followers=(array) $cb->followers_ids("id='".$userId."'");
-	var_dump($followers);
+/* Create a TwitterOauth object with consumer/user tokens. */
+$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
+
+/* If method is set change API call made. Test is called by default. */
+$content = $connection->get('account/verify_credentials');
+
+/* Some example calls */
+//$connection->get('users/show', array('screen_name' => 'abraham'));
+//$connection->post('statuses/update', array('status' => date(DATE_RFC822)));
+//$connection->post('statuses/destroy', array('id' => 5437877770));
+//$connection->post('friendships/create', array('id' => 9436992));
+//$connection->post('friendships/destroy', array('id' => 9436992));
+
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <title>Twitter OAuth in PHP</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    <style type="text/css">
+      img {border-width: 0}
+      * {font-family:'Lucida Grande', sans-serif;}
+    </style>
+  </head>
+  <body>
+    <div>
+      <h2>Welcome to a Twitter OAuth PHP example.</h2>
+
+      <p>This site is a basic showcase of Twitters OAuth authentication method. If you are having issues try <a href='./clearsessions.php'>clearing your session</a>.</p>
+
+      <p>
+        Links:
+        <a href='http://github.com/abraham/twitteroauth'>Source Code</a> &amp;
+        <a href='http://wiki.github.com/abraham/twitteroauth/documentation'>Documentation</a> |
+        Contact @<a href='http://twitter.com/abraham'>abraham</a>
+      </p>
+      <hr />
+      <?php if (isset($menu)) { ?>
+        <?php echo $menu; ?>
+      <?php } ?>
+    </div>
+    <?php if (isset($status_text)) { ?>
+      <?php echo '<h3>'.$status_text.'</h3>'; ?>
+    <?php } ?>
+    <p>
+      <pre>
+        <?php print_r($content); ?>
+      </pre>
+    </p>
+
+  </body>
+</html>
