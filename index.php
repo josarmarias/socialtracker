e76265a1f1843acf52a609db087677eeb470a341
Howard Chung <howardc93@gmail.com>
2013-04-10 13:46:37 -0400

    teset

--- index.php
+++ index.php
@@ -19,9 +19,6 @@ $access_token = $_SESSION['access_token'];
 /* Create a TwitterOauth object with consumer/user tokens. */
 $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
 
-
-//Do the DB connection here
-
 /* If method is set change API call made. Test is called by default. */
 $content = $connection->get('account/verify_credentials');
 $contentArray=(array) $content;
@@ -31,12 +28,45 @@ $realName=$contentArray['name'];
 echo $userId."<br>";
 echo $realName."<br>";
 
+//connect to db
+$dsn = "pgsql:"
+    . "host=ec2-23-21-91-97.compute-1.amazonaws.com;"
+    . "dbname=d79rcco6km5fni;"
+    . "user=qrcbvngsucryjg;"
+    . "port=5432;"
+    . "sslmode=require;"
+    . "password=iNwOAXNyk0KurrBt9hwyxd_x_D";
+$db = new PDO($dsn);
+
+$savedFollowers=[];
+//get existing db content
+$query = "SELECT followerid FROM followers WHERE userid = '$userId';";
+                                  $numberSaved=0;
+                                	$result = $db->query($query);
+                                	while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                                            $numberSaved=$numberSaved+1;
+                                            $savedFollowers[]=$row["followerid"];          
+                                }
+$result->closeCursor();
+
+//get current followers from twitter
 $content= $connection->get('followers/ids', array('id' => $userId));
 $contentArray=(array) $content;
-
 $followerIds=$contentArray['ids'];
+
+echo "Found x followers in database.";
+echo "You currently have y followers.";
+
+//two arrays, one of saved ids and one of current ids
+//find the difference, one is new followers and one is no longer following
+
+
+echo "New followers:<br>";
+
+echo "No longer followers:<br>";
+
+//get the names of these followers and display
 foreach($followerIds as &$followerId){
-echo $followerId."<br>";
 //get name given id
 $content= $connection->get('users/show', array('id' => $followerId));
 $contentArray=(array) $content;
@@ -44,20 +74,16 @@ $followerName=$contentArray['name'];
 echo $followerName."<br>";
 }
 
+//dump old db values and add new ones
+//$sql="INSERT INTO entries (fbid, class) VALUES ('$_POST[fbid]','$subjectAndSection')";
+// Performs the $sql query on the server to insert the values
+//$db->query($sql);
  ?>
 
-<p>
-Found x followers in database.
-You currently have y followers.
-</p>
 
-<p>
-New followers:
-</p>
 
-<p>
-No longer followers:
-</p>
+
+
 
  <p>
  <a href="./clearsessions.php">Clear Sessions</a>
